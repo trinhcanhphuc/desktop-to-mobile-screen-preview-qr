49fd97e3e70203e76cee0143439c33d99678d017
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-07-14 23:41:38 +0700

    refactor code

--- assets/js/utils/social_share.js
+++
@@ -1,9 +0,0 @@
-function getFacebookShareUrl(current_url) {
-  var fb_share_url = 'http://www.facebook.com/sharer.php?u=';
-  return fb_share_url + current_url;
-}
-
-function getPinterestShareUrl(current_url) {
-  var pinterest_share_url = 'https://www.pinterest.com/pin/create/button/?url=';
-  return pinterest_share_url + current_url;  
-}

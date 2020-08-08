61010689c539defce1659e25847c59ec84fcfc88
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-08-08 10:29:06 +0700

    Make to store

--- desktop-to-mobile-screen-preview-qr-store/assets/js/popup.js
+++ desktop-to-mobile-screen-preview-qr-store/assets/js/popup.js
@@ -1,1 +1,116 @@
-var fore_bg,back_bg,img_size,is_transparnet_bg,select_input_type="url";const PHONE_NUMER_PATTERN=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;function selectInputType(){return $("#select-input-type")}function QRCodeTextElement(){return $("#qrcode-text")}function QRCodeImageElement(){return $("#qrcode-img")}function generateQRCode(e){e=e||selectInputType().val();var t=QRCodeTextElement().val();switch(e){case"url":generateQRCodeByURL(t);break;case"text":generateQRCodeByText(t);break;case"phone":generateQRCodeByPhone(t)}}function generateQRCodeByURL(e){var t=QRCodeImageElement();t.html(""),renderQR(t,img_size,e),QRCodeTextElement().val(e)}function generateQRCodeByText(e){generateQRCodeByURL(e)}function generateQRCodeByPhone(e){generateQRCodeByURL(e)}function saveQRCode(){var e=document.createElement("a");e.href=QRCodeImageElement().find("img")[0].src,e.download="qr_code.png",e.click()}function renderQR(e,t,n){var r="0";"#ffffff"!=back_bg&&(r="1"),is_transparnet_bg&&(back_bg=null),e.qrcode(qrObjectBuilder(t,fore_bg,n,back_bg,r))}function qrObjectBuilder(e,t,n,r,o,a){var c="image";a&&(c="canvas");var d={render:c,size:e,fill:t,text:n,background:r,quiet:o,ecLevel:"L"};return d}$((function(){img_size=300,getCurrentTabUrl().then(e=>{generateQRCodeByURL(e)}),QRCodeTextElement().focus().select(),$("#btn_save_qr").click(()=>saveQRCode())})),QRCodeTextElement().on("input",(function(e){var t=$("#input-error");"phone"===select_input_type&&0==PHONE_NUMER_PATTERN.test(QRCodeTextElement().val())?t.show():t.hide(),generateQRCode()})),selectInputType().change(e=>{$("#input-error").hide(),generateQRCode(select_input_type=e.target.value),QRCodeTextElement().val(""),QRCodeTextElement().focus().select()});
+var fore_bg;
+var back_bg;
+var img_size;
+var is_transparnet_bg;
+var select_input_type = 'url';
+const PHONE_NUMER_PATTERN = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+// init extension
+$(function () {
+  img_size = 300;
+  getCurrentTabUrl().then((current_url) => {
+    generateQRCodeByURL(current_url);
+  });
+  QRCodeTextElement().focus().select();
+
+  $('#btn_save_qr').click(() => saveQRCode());
+});
+
+// trigger element handling
+QRCodeTextElement().on('input', function (e) {
+  var text_error_ele = $('#input-error');
+  if (select_input_type === 'phone' && PHONE_NUMER_PATTERN.test(QRCodeTextElement().val()) == false)
+    text_error_ele.show();
+  else
+    text_error_ele.hide();
+
+  generateQRCode();
+});
+
+selectInputType().change((e) => {
+  $('#input-error').hide();
+  select_input_type = e.target.value;
+  generateQRCode(select_input_type);
+  QRCodeTextElement().val('');
+  QRCodeTextElement().focus().select();
+});
+
+// get elements
+function selectInputType() {
+  return $('#select-input-type');
+}
+
+function QRCodeTextElement() {
+  return $('#qrcode-text');
+}
+
+function QRCodeImageElement() {
+  return $('#qrcode-img');
+}
+
+// functions
+function generateQRCode(generate_type) {
+  var generate_type = generate_type ? generate_type : selectInputType().val();
+  var input_value = QRCodeTextElement().val();
+  switch(generate_type){
+    case 'url':
+      generateQRCodeByURL(input_value);
+      break;
+    case 'text':
+      generateQRCodeByText(input_value);
+      break;
+    case 'phone':
+      generateQRCodeByPhone(input_value);
+      break;
+  }
+}
+
+function generateQRCodeByURL(url) {
+  var qr_img_ele = QRCodeImageElement();
+  qr_img_ele.html('');
+  renderQR(qr_img_ele, img_size, url);
+  QRCodeTextElement().val(url);
+}
+
+function generateQRCodeByText(text) {
+  generateQRCodeByURL(text);
+}
+
+function generateQRCodeByPhone(phone) {
+  generateQRCodeByURL(phone);
+}
+
+function saveQRCode() {
+  var qr_tag = document.createElement('a');
+  qr_tag.href = QRCodeImageElement().find('img')[0].src;
+  qr_tag.download = 'qr_code.png';
+  qr_tag.click();
+}
+
+function renderQR($el, the_size, the_text) {
+  var quiet = '0';
+  if (back_bg != '#ffffff') {
+    quiet = '1';
+  }
+  if (is_transparnet_bg) {
+    back_bg = null;
+  }
+  $el.qrcode(qrObjectBuilder(the_size, fore_bg, the_text, back_bg, quiet));
+}
+
+function qrObjectBuilder(s, f, t, b, q, c) {
+  var r = 'image';
+  if (c) {
+    r = 'canvas';
+  }
+  var o = {
+    render: r,
+    size: s,
+    fill: f,
+    text: t,
+    background: b,
+    quiet: q,
+  };
+  o.ecLevel = 'L';
+  return o;
+}
--- desktop-to-mobile-screen-preview-qr-store/assets/js/utils/browser_tab.js
+++ desktop-to-mobile-screen-preview-qr-store/assets/js/utils/browser_tab.js
@@ -1,1 +1,38 @@
-function getCurrentTabUrl(){return new Promise((r,e)=>{try{getCurrentTab({active:!0,currentWindow:!0}).then(e=>{r(e.url)})}catch(r){e(r)}})}function getCurrentTab(){return new Promise((r,e)=>{try{queryBrowserTabs({active:!0,currentWindow:!0}).then(e=>{r(e[0])})}catch(r){e(r)}})}function queryBrowserTabs(r){return new Promise((e,t)=>{try{chrome.tabs.query(r,(function(r){e(r)}))}catch(r){t(r)}})}
+function getCurrentTabUrl() {
+  var promise = new Promise((resolve, reject) => {
+    try {
+      getCurrentTab({ active: true, currentWindow: true }).then((current_tab) => {
+        resolve(current_tab.url);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  })
+  return promise;
+}
+
+function getCurrentTab() {
+  var promise = new Promise((resolve, reject) => {
+    try {
+      queryBrowserTabs({ active: true, currentWindow: true }).then((tabs) => {
+        resolve(tabs[0]);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  })
+  return promise;
+}
+
+function queryBrowserTabs(query) {
+  var promise = new Promise((resolve, reject) => {
+    try {
+      chrome.tabs.query(query, function (tabs) {
+        resolve(tabs);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+  return promise;
+}
